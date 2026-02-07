bcc269983fab32c08aab9b5729a97de438139772
Basavrajcharate <basavraj.charate130904@gmail.com>
2026-02-08 02:48:20 +0530

    Create Navbar

---
+++ Notes_app/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav>
+        <div className="logo">NoteX</div>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+            <li>Services</li>
+            <li>Login</li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar
