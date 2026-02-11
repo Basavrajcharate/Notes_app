0c093a0ca1d06c20acee137db95ca49122393acc
Basavrajcharate <basavraj.charate130904@gmail.com>
2026-02-11 23:41:43 +0530

    footer changes

--- Notes_app/components/Footer.jsx
+++ Notes_app/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='footer'>NoteX App Created By Basavraj</div>
+    <div className='footer'>NoteX App Created By <a href="https://www.linkedin.com/in/basavraj-charate-a01161297/">Basavraj</a></div>
   )
 }
 
